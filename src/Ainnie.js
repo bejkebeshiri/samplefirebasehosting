d14ffce181749da45c2a635fbae9beeb6f9d9a5c
bejke beshiri <bejke.beshiri@'dpschool.io>
2020-05-13 02:52:24 +0200

    test

--- src/Ainnie.js
+++ src/Ainnie.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
- 
+
 class Main extends Component {
   render() {
     return (
         <div>
             <div className="content">
-       
-<h2>Mahnoor Fatima</h2>
-        <p>Hello, I am Mahnoor from Pakistan, I love to code and travel! I am CS Grad from the best university of Pakistan, loving DPS</p>
- 
+
+<h2>Annie Hall</h2>
+<img src= {Img} alt="pic"/>
+        <p>Hello, I am an interaction designer who sees design opportunities in everything surrounding us.
+         I’ve previously interned at North Kingdom, served as a formal media designer at yU+[Co} lab, and studied at Savannah College of Arts and Design, and Umea Insitute of Design. Now, living the Nordic lifestyle while crafting knowledge in design research, usability testing and interaction experiences. When I am free, I also enjoy co-hosting a creative podcast The Accent Studio , taking photos, creative coding and performing audiovisual art with musicians </p>
+
 
 </div>
         </div>
     );
   }
 }
- 
+
 export default Main;
--- src/Main.js
+++ src/Main.js
@@ -7,7 +7,7 @@ import {
 import Home from "./Home";
 import Mahnoor from "./Mahnoor";
 import Alok from "./Alok";
- 
+
 import Ainnie from "./Ainnie";
 import bejki from "./bejki";
 
@@ -18,7 +18,7 @@ import Sowmya from "./Sowmya";
 class Main extends Component {
   render() {
     return (
-    <HashRouter> 
+    <HashRouter>
        <div>
           <h1>Team Digital Hot Mess</h1>
           <ul className="header">
@@ -26,18 +26,18 @@ class Main extends Component {
             <li><NavLink to="/Mahnoor">Mahnoor</NavLink></li>
             <li><NavLink to="/Alok">Alok</NavLink></li>
 
-            <li><NavLink to="/Ainnie">Ainnie</NavLink></li>
-            <li><NavLink to="/bejki">bejki</NavLink></li>
+            <li><NavLink to="/Ainnie">Annie</NavLink></li>
+            <li><NavLink to="/bejki">Bejke</NavLink></li>
 
-            <li><NavLink to="/Sakhshi">Sakhshi</NavLink></li>
-            <li><NavLink to="/Jorenz">Jorenz</NavLink></li>
+            <li><NavLink to="/Sakhshi">Sakshi</NavLink></li>
+            <li><NavLink to="/Jorenz">Jorn</NavLink></li>
             <li><NavLink to="/Sowmya">Sowmya</NavLink></li>
           </ul>
           <div className="content">
             <Route path="/" component={Home}/>
             <Route path="/Mahnoor" component={Mahnoor}/>
             <Route path="/Alok" component={Alok}/>
-            <Route path="/Ainnie" component={Ainnie}/> 
+            <Route path="/Ainnie" component={Ainnie}/>
             <Route path="/bejki" component={bejki}/>
             <Route path="/Sakhshi" component={Sakhshi}/>
             <Route path="/Jorenz" component={Jorenz}/>
@@ -48,5 +48,5 @@ class Main extends Component {
     );
   }
 }
- 
+
 export default Main;
